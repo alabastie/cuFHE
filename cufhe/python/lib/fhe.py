cbd10ec544e7b898b2d103ba705687b41d157a28
ChobbyRace <RobC697@gmail.com>
2018-09-25 20:14:27 -0400

    Testing our multiplier

--- cufhe/python/lib/fhe.py
+++ cufhe/python/lib/fhe.py
@@ -7,11 +7,11 @@
 
 # Attempt to import GPU module, fallback to CPU module
 try:
-    import lib.fhepy_gpu as fhe
-    use_gpu = True
+	import lib.fhepy_gpu as fhe
+	use_gpu = True
 except:
-    import lib.fhepy_cpu as fhe
-    use_gpu = False
+	import lib.fhepy_cpu as fhe
+	use_gpu = False
 
 import time
 import timeit
@@ -19,321 +19,322 @@ from multiprocessing import process
 from multiprocessing import Pool
 
 def UseGPU():
-    return use_gpu
+	return use_gpu
 
 def LoadPubKey(pubfile="pubkey.txt"):
-    pubkey = fhe.PubKey()
-    fhe.ReadPubKeyFromFile(pubkey, pubfile)
-    return pubkey
+	pubkey = fhe.PubKey()
+	fhe.ReadPubKeyFromFile(pubkey, pubfile)
+	return pubkey
 
 def LoadPriKey(prifile="prikey.txt"):
-    prikey = fhe.PriKey()
-    fhe.ReadPriKeyFromFile(prikey, prifile)
-    return prikey
+	prikey = fhe.PriKey()
+	fhe.ReadPriKeyFromFile(prikey, prifile)
+	return prikey
 
 def LoadKeys(pubfile="pubkey.txt", prifile="prikey.txt"):
-    return LoadPubKey(), LoadPriKey()
+	return LoadPubKey(), LoadPriKey()
 
 def StorePubKey(pubkey, pubfile="pubkey.txt"):
-    fhe.WritePubKeyToFile(pubkey, pubfile)
-    print("Public key is stored in ./" + pubfile)
+	fhe.WritePubKeyToFile(pubkey, pubfile)
+	print("Public key is stored in ./" + pubfile)
 
 def StorePriKey(prikey, prifile="prikey.txt"):
-    fhe.WritePriKeyToFile(prikey, prifile)
-    print("Private key is stored in ./" + prifile)
+	fhe.WritePriKeyToFile(prikey, prifile)
+	print("Private key is stored in ./" + prifile)
 
 def StoreKeys(pubkey, prikey, pubfile="pubkey.txt", prifile="prikey.txt"):
-    StorePubKey(pubkey, pubfile)
-    StorePriKey(prikey, prifile)
+	StorePubKey(pubkey, pubfile)
+	StorePriKey(prikey, prifile)
 
 def PriKeyGen():
-    prikey = fhe.PriKey()
-    fhe.PriKeyGen(prikey)
-    return prikey
+	prikey = fhe.PriKey()
+	fhe.PriKeyGen(prikey)
+	return prikey
 
 def PubKeyGen(prikey):
-    pubkey = fhe.PubKey()
-    fhe.PubKeyGen(pubkey, prikey)
-    return pubkey
+	pubkey = fhe.PubKey()
+	fhe.PubKeyGen(pubkey, prikey)
+	return pubkey
 
 def KeyGen():
-    prikey = PriKeyGen()
-    return PubKeyGen(prikey), prikey
+	prikey = PriKeyGen()
+	return PubKeyGen(prikey), prikey
 
 def Init(pubkey):
-    if use_gpu:
-        fhe.Initialize(pubkey)
-    else:
-        pass
+	if use_gpu:
+		fhe.Initialize(pubkey)
+	else:
+		pass
 
 def PtxtMod():
-    return fhe.Ptxt().PtxtSpace
+	return fhe.Ptxt().PtxtSpace
 
 def Encrypt(ptxt, prikey, count=1, pubkey=None):
-    if pubkey is None:
-        pubkey = PubKeyGen(prikey)
-    if isinstance(ptxt, (int, long)):
-        msg = ptxt
-        ptxt = fhe.Ptxt()
-        if count == 1:
-            ptxt.message = msg;
-            ctxt = Ctxt(pubkey)
-            fhe.Encrypt(ctxt.ctxt_, ptxt, prikey)
-            return ctxt
+	if pubkey is None:
+		pubkey = PubKeyGen(prikey)
+	if isinstance(ptxt, (int, long)):
+		msg = ptxt
+		ptxt = fhe.Ptxt()
+		if count == 1:
+			ptxt.message = msg;
+			ctxt = Ctxt(pubkey)
+			fhe.Encrypt(ctxt.ctxt_, ptxt, prikey)
+			return ctxt
 
-        msg_bin = bin(msg)[2:].zfill(count)
-        msg_list = []
-        ct = CtxtList(count, pubkey)
-        for i in range(count):
-            ptxt.message = int(msg_bin[i], 2)
-            fhe.Encrypt(ct.ctxts_[count - i - 1].ctxt_, ptxt, prikey)
-        return ct
+		msg_bin = bin(msg)[2:].zfill(count)
+		msg_list = []
+		ct = CtxtList(count, pubkey)
+		for i in range(count):
+			ptxt.message = int(msg_bin[i], 2)
+			fhe.Encrypt(ct.ctxts_[count - i - 1].ctxt_, ptxt, prikey)
+		return ct
 
 def Decrypt(ctxt, prikey):
-    ptxt = fhe.Ptxt()
-    if isinstance(ctxt, Ctxt):
-        fhe.Decrypt(ptxt, ctxt.ctxt_, prikey)
-        return ptxt.message
+	ptxt = fhe.Ptxt()
+	if isinstance(ctxt, Ctxt):
+		fhe.Decrypt(ptxt, ctxt.ctxt_, prikey)
+		return ptxt.message
 
-    if isinstance(ctxt, CtxtList):
-        ptxt_list = ""
-        for c in reversed(ctxt.ctxts_):
-            fhe.Decrypt(ptxt, c.ctxt_, prikey)
-            ptxt_list += str(ptxt.message)
-        return int(ptxt_list, 2)
+	if isinstance(ctxt, CtxtList):
+		ptxt_list = ""
+		for c in reversed(ctxt.ctxts_):
+			fhe.Decrypt(ptxt, c.ctxt_, prikey)
+			ptxt_list += str(ptxt.message)
+		return int(ptxt_list, 2)
 
 def SetSeed():
-    fhe.SetSeed(int(time.time()))
+	fhe.SetSeed(int(time.time()))
 
 def Synchronize():
-    if use_gpu:
-        fhe.Synchronize()
-    else:
-    	# if (pinit == 1)
-    	# 	p.close()
-    	# 	p.join()
-    	# 	p = Pool()
-    	# else:
-    	# 	p = Pool()
-        pass
+	if use_gpu:
+		fhe.Synchronize()
+	else:
+		# if (pinit == 1)
+		# 	p.close()
+		# 	p.join()
+		# 	p = Pool()
+		# else:
+		# 	p = Pool()
+		pass
 
 def AND(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.AND(result, input1, input2, stream)
-    else:
-    	#result1 = p.map(fhe.AND, args=(result, input1, input2, pubkey)) #may need a ctx.
-    	#p1.start()
-        fhe.AND(result, input1, input2, pubkey)
+	if use_gpu:
+		fhe.AND(result, input1, input2, stream)
+	else:
+		#result1 = p.map(fhe.AND, args=(result, input1, input2, pubkey)) #may need a ctx.
+		#p1.start()
+		fhe.AND(result, input1, input2, pubkey)
 
 def NAND(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.NAND(result, input1, input2, stream)
-    else:
-        fhe.NAND(result, input1, input2, stream)
+	if use_gpu:
+		fhe.NAND(result, input1, input2, stream)
+	else:
+		fhe.NAND(result, input1, input2, stream)
 
 def OR(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.OR(result, input1, input2, stream)
-    else:
-    	#p3 = process( target = fhe.OR, args = (result, input1, input2, stream))
-        fhe.OR(result, input1, input2, pubkey)
-        #p3.start()
+	if use_gpu:
+		fhe.OR(result, input1, input2, stream)
+	else:
+		#p3 = process( target = fhe.OR, args = (result, input1, input2, stream))
+		fhe.OR(result, input1, input2, pubkey)
+		#p3.start()
 
 def NOR(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.NOR(result, input1, input2, stream)
-    else:
-        fhe.NOR(result, input1, input2, pubkey)
+	if use_gpu:
+		fhe.NOR(result, input1, input2, stream)
+	else:
+		fhe.NOR(result, input1, input2, pubkey)
 
 def XOR(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.XOR(result, input1, input2, stream)
-    else:
-    	#p2 = process(target = fhe.XOR, args = (result, input1, input2, pubkey)) #may need ctx. like the AND
-        fhe.XOR(result, input1, input2, pubkey)
-        #p2.start()
+	if use_gpu:
+		fhe.XOR(result, input1, input2, stream)
+	else:
+		#p2 = process(target = fhe.XOR, args = (result, input1, input2, pubkey)) #may need ctx. like the AND
+		fhe.XOR(result, input1, input2, pubkey)
+		#p2.start()
 
 def XNOR(result, input1, input2, stream=None, pubkey=None):
-    if use_gpu:
-        fhe.XNOR(result, input1, input2, stream)
-    else:
-        fhe.XNOR(result, input1, input2, pubkey)
+	if use_gpu:
+		fhe.XNOR(result, input1, input2, stream)
+	else:
+		fhe.XNOR(result, input1, input2, pubkey)
 
 def NOT(result, input1, stream=None):
-    if use_gpu:
-        fhe.NOT(result, input1, stream)
-    else:
-        fhe.NOT(result, input1)
+	if use_gpu:
+		fhe.NOT(result, input1, stream)
+	else:
+		fhe.NOT(result, input1)
 
 
 class Stream:
-    def __init__(self):
-        if use_gpu:
-            self.stream = fhe.Stream()
-        else:
-            self.stream = None
+	def __init__(self):
+		if use_gpu:
+			self.stream = fhe.Stream()
+		else:
+			self.stream = None
 
-    def Create(self):
-        if use_gpu:
-            self.stream.Create()
-        else:
-            pass
+	def Create(self):
+		if use_gpu:
+			self.stream.Create()
+		else:
+			pass
 
-        return self.stream
+		return self.stream
 
 
 class Ctxt:
-    def __init__(self, pubkey=None, zero = False):
-        self.ctxt_ = fhe.Ctxt()
-        self.pubkey_ = pubkey
-        if zero:
-        	ctxt_2 = Ctxt(self.pubkey_)
-        	print(self.pubkey_)
-        	print (ctxt_2.pubkey_)
-        	NOT(ctxt_2.ctxt_, self.ctxt_)	#I know that i cant do the .ctxt_2 thing but its just supposed to be a temp variable
-        	print type(self.pubkey_)
-        	AND(self.ctxt_, self.ctxt_, ctxt_2.ctxt_, pubkey = pubkey)
+	def __init__(self, pubkey=None, zero = False):
+		self.ctxt_ = fhe.Ctxt()
+		self.pubkey_ = pubkey
+		if zero:
+			ctxt_2 = Ctxt(self.pubkey_)
+			print(self.pubkey_)
+			print (ctxt_2.pubkey_)
+			NOT(ctxt_2.ctxt_, self.ctxt_)	#I know that i cant do the .ctxt_2 thing but its just supposed to be a temp variable
+			print type(self.pubkey_)
+			AND(self.ctxt_, self.ctxt_, ctxt_2.ctxt_, pubkey = pubkey)
 
-    def Decrypt(self, prikey):
-        return Decrypt(self, prikey)
+	def Decrypt(self, prikey):
+		return Decrypt(self, prikey)
 
-    def Encrypt(self, msg, prikey):
-        Encrypt(msg, prikey, self.pubkey_)
-        return self
+	def Encrypt(self, msg, prikey):
+		Encrypt(msg, prikey, self.pubkey_)
+		return self
 
-    def __and__(self, other):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        AND(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
-        Synchronize()
-        return result
+	def __and__(self, other):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		AND(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
+		Synchronize()
+		return result
 
-    def __xor__(self, other):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        XOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
-        Synchronize()
-        return result
+	def __xor__(self, other):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		XOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
+		Synchronize()
+		return result
 
-    def __or__(self, other):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        OR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
-        Synchronize()
-        return result
+	def __or__(self, other):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		OR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
+		Synchronize()
+		return result
 
-    def __invert__(self):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        NOT(result.ctxt_, self.ctxt_, st)
-        Synchronize()
-        return result
+	def __invert__(self):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		NOT(result.ctxt_, self.ctxt_, st)
+		Synchronize()
+		return result
 
-    def __eq__(self, other):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        XNOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
-        Synchronize()
-        return result
+	def __eq__(self, other):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		XNOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
+		Synchronize()
+		return result
 
-    def __ne__(self, other):
-        result = Ctxt(self.pubkey_)
-        st = Stream().Create()
-        Synchronize()
-        XOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
-        Synchronize()
-        return result
+	def __ne__(self, other):
+		result = Ctxt(self.pubkey_)
+		st = Stream().Create()
+		Synchronize()
+		XOR(result.ctxt_, self.ctxt_, other.ctxt_, st, self.pubkey_)
+		Synchronize()
+		return result
 
-    def __lt__(self, other):
-        return ~self & other
+	def __lt__(self, other):
+		return ~self & other
 
-    def __le__(self, other):
-        return ~self | other
+	def __le__(self, other):
+		return ~self | other
 
-    def __gt__(self, other):
-        return self & ~other
+	def __gt__(self, other):
+		return self & ~other
 
-    def __ge__(self, other):
-        return self | ~other
+	def __ge__(self, other):
+		return self | ~other
 
 
 class CtxtList:
-    def __init__(self, length=0, pubkey=None):
-        self.ctxts_ = [Ctxt(pubkey) for i in range(length)]
-        self.pubkey_ = pubkey
+	def __init__(self, length=0, pubkey=None, zero = False):
+		self.ctxts_ = [Ctxt(pubkey, zero=zero) for i in range(length)]
+		self.pubkey_ = pubkey
 
-    def Decrypt(self, prikey):
-        return Decrypt(self, prikey)
 
-    def __and__(self, other):
-        result = CtxtList(len(self.ctxts_), self.pubkey_)
-        st = [Stream().Create() for i in range(len(self.ctxts_))]
-        Synchronize()
-        for i in range(len(self.ctxts_)):
-            AND(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
-        Synchronize()
-        return result
+	def Decrypt(self, prikey):
+		return Decrypt(self, prikey)
 
-    def __xor__(self, other):
-        result = CtxtList(len(self.ctxts_), self.pubkey_)
-        st = [Stream().Create() for i in range(len(self.ctxts_))]
-        Synchronize()
-        for i in range(len(self.ctxts_)):
-            XOR(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
-        Synchronize()
-        return result
+	def __and__(self, other):
+		result = CtxtList(len(self.ctxts_), self.pubkey_)
+		st = [Stream().Create() for i in range(len(self.ctxts_))]
+		Synchronize()
+		for i in range(len(self.ctxts_)):
+			AND(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
+		Synchronize()
+		return result
 
-    def __or__(self, other):
-        result = CtxtList(len(self.ctxts_), self.pubkey_)
-        st = [Stream().Create() for i in range(len(self.ctxts_))]
-        Synchronize()
-        for i in range(len(self.ctxts_)):
-            OR(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
-        Synchronize()
-        return result
+	def __xor__(self, other):
+		result = CtxtList(len(self.ctxts_), self.pubkey_)
+		st = [Stream().Create() for i in range(len(self.ctxts_))]
+		Synchronize()
+		for i in range(len(self.ctxts_)):
+			XOR(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
+		Synchronize()
+		return result
 
-    def __invert__(self):
-        result = CtxtList(len(self.ctxts_), self.pubkey_)
-        st = [Stream().Create() for i in range(len(self.ctxts_))]
-        Synchronize()
-        for i in range(len(self.ctxts_)):
-            AND(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, st[i], self.pubkey_)
-        Synchronize()
-        return result
+	def __or__(self, other):
+		result = CtxtList(len(self.ctxts_), self.pubkey_)
+		st = [Stream().Create() for i in range(len(self.ctxts_))]
+		Synchronize()
+		for i in range(len(self.ctxts_)):
+			OR(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[i], self.pubkey_)
+		Synchronize()
+		return result
 
-    # ripple carry adder
-    def __add__(self, other):
-        x = CtxtList(len(self.ctxts_), self.pubkey_)    # temporaries
-        y = CtxtList(len(self.ctxts_), self.pubkey_)
-        z = CtxtList(len(self.ctxts_), self.pubkey_)
-        c = CtxtList(len(self.ctxts_), self.pubkey_)    # carry
-        r = CtxtList(len(self.ctxts_), self.pubkey_)    # result
-        st = [Stream().Create() for i in range(2*len(self.ctxts_))]
+	def __invert__(self):
+		result = CtxtList(len(self.ctxts_), self.pubkey_)
+		st = [Stream().Create() for i in range(len(self.ctxts_))]
+		Synchronize()
+		for i in range(len(self.ctxts_)):
+			AND(result.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, st[i], self.pubkey_)
+		Synchronize()
+		return result
 
-        Synchronize()
+	# ripple carry adder
+	def __add__(self, other):
+		x = CtxtList(len(self.ctxts_), self.pubkey_)    # temporaries
+		y = CtxtList(len(self.ctxts_), self.pubkey_)
+		z = CtxtList(len(self.ctxts_), self.pubkey_)
+		c = CtxtList(len(self.ctxts_), self.pubkey_)    # carry
+		r = CtxtList(len(self.ctxts_), self.pubkey_)    # result
+		st = [Stream().Create() for i in range(2*len(self.ctxts_))]
 
-  		if use_gpu:
-  			XOR(r.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[0], self.pubkey_)
-  			AND(c.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[1], self.pubkey_)
-  		else:
-	  		p1 = Process(target =XOR, args = (r.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[0], self.pubkey_))
-	  		p2 = Process(target =AND, args = (c.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[1], self.pubkey_))
-	  		p1.start()
-	  		p2.start()
-	  		p1.join()
-	  		p2.join()
+		Synchronize()
+
+		if use_gpu:
+			XOR(r.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[0], self.pubkey_)
+			AND(c.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[1], self.pubkey_)
+		else:
+			p1 = Process(target =XOR, args = (r.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[0], self.pubkey_))
+			p2 = Process(target =AND, args = (c.ctxts_[0].ctxt_, self.ctxts_[0].ctxt_, other.ctxts_[0].ctxt_, st[1], self.pubkey_))
+			p1.start()
+			p2.start()
+			p1.join()
+			p2.join()
    
-        Synchronize()
+		Synchronize()
 #		.........................This is where we would call the two functions an implement them into the pool...................
 		if use_gpu:
 			for i in range(1, len(self.ctxts_)):
-           		XOR(x.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i], self.pubkey_)
-            	AND(y.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i+1], self.pubkey_)
+				XOR(x.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i], self.pubkey_)
+				AND(y.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i+1], self.pubkey_)
 		else:#creates a pool and then put its in a map to compute the for loop
 			p = Pool()
 			p.map(SynchFor, self.ctxts_)
@@ -341,21 +342,29 @@ class CtxtList:
 			p.join()
 
 
-        Synchronize()
-        for i in range(1, len(self.ctxts_)):
-            AND(z.ctxts_[i-1].ctxt_, x.ctxts_[i].ctxt_, c.ctxts_[i-1].ctxt_, st[0], self.pubkey_)
-            XOR(r.ctxts_[i].ctxt_, x.ctxts_[i].ctxt_, c.ctxts_[i-1].ctxt_, st[1], self.pubkey_)
-            #aboe two can use a map
-            #we need some way to break this up or somehow figure out a way to make synchronize work for our shit
-            Synchronize()
-            OR(c.ctxts_[i].ctxt_, z.ctxts_[i-1].ctxt_, y.ctxts_[i].ctxt_, st[0], self.pubkey_)
-            Synchronize()
-        return r
-       #--------------------------------My Makeshift For loop for the p.map pool --------------------------------------------
-    def SynchFor(self.ctxts_):
-    	for i in range(1, len(self.ctxts_)):
-            XOR(x.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i], self.pubkey_)
-            AND(y.ctxts_[i].ctxt_, self.ctxts_[i].ctxt_, other.ctxts_[i].ctxt_, st[2*i+1], self.pubkey_)
+		Synchronize()
+		for i in range(1, len(self.ctxts_)):
+			AND(z.ctxts_[i-1].ctxt_, x.ctxts_[i].ctxt_, c.ctxts_[i-1].ctxt_, st[0], self.pubkey_)
+			XOR(r.ctxts_[i].ctxt_, x.ctxts_[i].ctxt_, c.ctxts_[i-1].ctxt_, st[1], self.pubkey_)
+			#aboe two can use a map
+			#we need some way to break this up or somehow figure out a way to make synchronize work for our shit
+			Synchronize()
+			OR(c.ctxts_[i].ctxt_, z.ctxts_[i-1].ctxt_, y.ctxts_[i].ctxt_, st[0], self.pubkey_)
+			Synchronize()
+		return r
+
+	def __mul__(self, other):
+		temp = [CtxtList(2*len(self.ctxts_), self.pubkey_, zero = True) for i in range(len(self.ctxts_))]
+
+		for i in range (len(self.ctxt)):
+			for j in range (len(self.ctxt)):
+				AND(temp[i][j+i].ctxt_, self.ctxts_[j].ctxt_, other.ctxts_[i].ctxt_, None, self.pubkey_)
+
+	 
+		for i in range (1, len(self.ctxt)) :
+			temp[0] = temp[0] + temp[i]
+
+		return temp[0]
 
 def test0 (pubkey_):
 	c = Ctxt(pubkey = pubkey_, zero = True)
